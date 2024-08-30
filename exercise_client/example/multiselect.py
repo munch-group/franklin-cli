47faaca9c14e76bb80e833f1b1f9f52907d13187
Kasper Munch <kaspermunch@birc.au.dk>
2024-08-30 18:16:48 +0200

    Attemping auto download of repo

--- exercise_client/example/multiselect.py
+++
@@ -1,6 +0,0 @@
-from pick import pick
-
-title = "Choose your favorite programming language(use space to select)"
-options = ["Java", "JavaScript", "Python", "PHP", "C++", "Erlang", "Haskell"]
-selected = pick(options, title, multiselect=True, min_selection_count=1)
-print(selected)
