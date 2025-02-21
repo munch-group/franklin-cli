aea152a859e40e0740ed78a6f9295e0721d94e01
Kasper Munch <kaspermunch@birc.au.dk>
2025-02-21 23:04:51 +0100

    update

--- franklin/jupyter.py
+++ franklin/jupyter.py
@@ -190,9 +190,11 @@ def launch_exercise():
     # docker_run_p = Popen(shlex.split(cmd), 
     #                      stdout=DEVNULL, stderr=DEVNULL, 
     #                      **popen_kwargs)
-    print(utils.format_cmd(cmd))
-    docker_run_p = Popen(utils.format_cmd(cmd), 
-                        #  stdout=DEVNULL, stderr=DEVNULL, 
+
+    cmd = cmd.split()
+    cmd[0] = shutil.which(cmd[0])
+    docker_run_p = Popen(cmd, 
+                        stdout=DEVNULL, stderr=DEVNULL, 
                          **popen_kwargs)
 
     time.sleep(5)
