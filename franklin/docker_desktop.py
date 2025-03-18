33227e70c77ca997e4f87040004a3b93c9ace284
Kasper Munch <kaspermunch@birc.au.dk>
2025-03-18 13:43:05 +0100

    Refactored code

---
+++ franklin/docker_desktop.py
@@ -0,0 +1,272 @@
+import sysconfig
+import os
+import re
+from . import terminal as term
+from . import utils
+import sys
+import requests
+import click
+import json
+from .config import WRAP_WIDTH, PG_OPTIONS
+from subprocess import check_output, DEVNULL, TimeoutExpired
+from .logger import logger
+import subprocess
+import time
+import shutil
+from packaging.version import Version, InvalidVersion
+
+
+def install_docker_desktop():
+
+    architecture = sysconfig.get_platform().split('-')[-1]
+    assert architecture
+
+    operating_system = utils.system()
+
+    if operating_system == 'Windows':
+        if architecture == 'arm64':
+            download_url = 'https://desktop.docker.com/win/main/arm64/Docker%20Desktop%20Installer.exe'
+        else:
+            download_url = 'https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe'
+        installer = 'Docker Desktop Installer.exe'
+    elif operating_system == 'Darwin':
+        if architecture == 'arm64':
+            download_url = 'https://desktop.docker.com/mac/main/arm64/Docker.dmg'
+        else:
+            download_url = 'https://desktop.docker.com/mac/main/amd64/Docker.dmg'
+        installer = 'Docker.dmg'
+    else:
+        url = 'https://docs.docker.com/desktop/linux/install/'
+        term.echo(f'Download from {url} and install before proceeding.')
+        sys.exit(1)
+
+    utils.boxed_text(f"Franklin needs Docker Desktop", 
+                     ['Franklin depends on a program called Docker Desktop and will download a Docker Desktop installer to your Downloads folder.'],
+                     prompt='Press Enter to start the download...', 
+                     fg='green')
+
+    response = requests.get(download_url, stream=True)
+    if not response.ok:
+        term.echo(f"Could not download Docker Desktop. Please download from {download_url} and install before proceeding.")
+        sys.exit(1)
+
+    file_size = response.headers['Content-length']
+    with open(installer, mode="wb") as file:
+        nr_chunks = int(file_size) // (10 * 1024) + 1
+        with click.progressbar(length=nr_chunks, label='Downloading:', **PG_OPTIONS) as bar:
+            for chunk in response.iter_content(chunk_size=10 * 1024):
+                file.write(chunk)
+                bar.update(1)
+    
+    if utils.system() == 'Windows':
+
+        kwargs = dict(subsequent_indent=5)
+        term.echo()
+        term.echo()
+        term.secho(f"How to install Docker Desktop on Windows:", fg='green')
+        term.secho('='*WRAP_WIDTH, fg='green')
+        term.echo()
+        term.echo("  Please follow this exact sequence of steps:")
+        term.echo()
+        term.echo('  1. Open the Downloads folder.', **kwargs)
+        term.echo('  2. Double-click the "Docker Desktop Installer.exe" file.', **kwargs)
+        term.echo('  3. Follow the default installation procedure.', **kwargs)
+        term.echo('  4. When the installation is completed, open Docker Desktop.', **kwargs)
+        term.echo('  5. Accept the Docker Desktop license agreement')
+        term.echo('  6. When you are asked to log in or create an account, just click skip.', **kwargs)
+        term.echo('  7. When you are asked to take a survey, just click skip.', **kwargs)
+        term.echo('  8. Wait while it says "Starting the Docker Engine..."')
+        term.echo('  9. If it says "New version available" in the bottom right corner, click that to update (scroll to find the blue button)"', **kwargs)
+        term.echo('  10. Quit the Docker application.', **kwargs)
+        term.echo('  11. Return to this window and start Franklin the same way as you did before.', **kwargs)
+        term.echo()
+        term.echo('  Press Enter now to close Franklin.')
+        term.echo()
+        term.echo('='*WRAP_WIDTH, fg='green')
+        click.pause('')
+        sys.exit(0)
+
+        # subprocess.run(installer, check=True)
+
+        # term.echo(" - Removing installer...")
+        # os.remove(installer)
+
+    elif utils.system() == 'Darwin':
+        cmd = utils.fmt_cmd(f'hdiutil attach -nobrowse -readonly {installer}')
+        output = check_output(cmd).decode().strip()
+
+        # Extract the mounted volume name from the output
+        mounted_volume_name = re.search(r'/Volumes/([^ ]*)', output.strip()).group(1)
+
+        term.echo("Installing:")
+        term.echo()
+        term.secho('='*WRAP_WIDTH, fg='red')
+        term.echo('  Press Enter and then drag the Docker to the Applications folder.', fg='red')
+        term.secho('='*WRAP_WIDTH, fg='red')
+        term.echo()
+        click.pause('Press Enter...')
+
+        check_output(utils.fmt_cmd(f'open /Volumes/{mounted_volume_name}')).decode().strip()
+
+        term.echo(" - Copying to Applications...")
+        prev_size = ''
+        for _ in range(1000):
+            cmd = f'du -s /Applications/Docker.app'
+            logger.debug(cmd)
+            cmd = cmd.split()
+            cmd[0] = shutil.which(cmd[0])
+            output = subprocess.run(cmd, stdout=PIPE, stderr=DEVNULL, timeout=1, check=False).stdout.decode().strip()
+            if output:
+                size = output.split()[0]
+                if size == prev_size:
+                    break
+                print(prev_size, size)
+                prev_size = size
+            time.sleep(5)
+
+        term.dummy_progressbar(seconds=10, label='Validating Docker Desktop:')
+
+        term.echo(" - Unmounting...")
+
+        if os.path.exists('/Volumes/{mounted_volume_name}'):
+            run(f'hdiutil detach /Volumes/{mounted_volume_name}/')
+
+        term.echo(" - Removing installer...")
+        os.remove(installer)
+
+        kwargs = dict(subsequent_indent=5)
+        term.echo()
+        term.echo()
+        term.secho(f"How to set up Docker Desktop on Mac:", fg='green')
+        term.secho('='*WRAP_WIDTH, fg='green')
+        term.echo()
+        term.echo("  Please follow this exact sequence of steps:")
+        term.echo()
+        term.echo('  1. Open the Docker application from your Applications folder', **kwargs)
+        term.echo('  3. Follow the default installation procedure.', **kwargs)
+        term.echo('  4. When the installation is completed, open Docker Desktop.', **kwargs)
+        term.echo('  5. Accept the Docker Desktop license agreement')
+        term.echo('  6. When you are asked to log in or create an account, just click skip.', **kwargs)
+        term.echo('  7. When you are asked to take a survey, just click skip.', **kwargs)
+        term.echo('  8. Wait while it says "Starting the Docker Engine..."')
+        term.echo('  9. If it says "New version available" in the bottom right corner, click that to update (scroll to find the blue button)"', **kwargs)
+        term.echo('  10. Quit the Docker application.', **kwargs)
+        term.echo('  11. Return to this window and start Franklin the same way as you did before.', **kwargs)
+        term.echo()
+        term.echo('  Press Enter now to close Franklin.')
+        term.echo()
+        term.echo('='*WRAP_WIDTH, fg='green')
+        click.pause('')
+        sys.exit(0)
+
+#  start /w "" "Docker Desktop Installer.exe" uninstall
+#  /Applications/Docker.app/Contents/MacOS/uninstall
+
+
+def failsafe_start_docker_desktop():
+
+    if not shutil.which('docker'):
+         install_docker_desktop()    
+
+    docker_desktop_start()
+
+    term.dummy_progressbar(seconds=10, label='Starting Docker Desktop:')
+
+    if not docker_desktop_status() == 'running':
+        term.secho("Could not start Docker Desktop. Please start Docker Desktop manually.", fg='red')
+        sys.exit(1)
+
+    if utils.system() == 'Darwin':
+        update_docker_desktop()
+
+
+def docker_desktop_restart():
+    cmd = 'docker desktop restart'
+    timeout=40    
+    try:
+        logger.debug(cmd)
+        output = check_output(utils.fmt_cmd(cmd), timeout=timeout).decode()
+    except TimeoutExpired as e:
+        logger.debug(f"Timeout of {timeout} seconds exceeded.")
+        return False
+    except subprocess.CalledProcessError as e:        
+        logger.debug(e.output.decode())
+        raise click.Abort()    
+    return True
+
+
+def docker_desktop_start():
+    cmd = 'docker desktop start'
+    timeout=40    
+    try:
+        logger.debug(cmd)
+        output = check_output(utils.fmt_cmd(cmd), timeout=timeout).decode()
+    except TimeoutExpired as e:
+        logger.debug(f"Timeout of {timeout} seconds exceeded.")
+        return False
+    except subprocess.CalledProcessError as e:        
+        logger.debug(e.output.decode())
+        raise click.Abort()    
+    return True
+
+
+def docker_desktop_stop():
+    # _command('docker desktop stop', silent=True)
+    utils.run_cmd('docker desktop stop', check=False)
+
+
+def docker_desktop_status():
+    # stdout = subprocess.run(utils._cmd('docker desktop status --format json'), 
+    #                         check=False, stderr=DEVNULL, stdout=PIPE).stdout.decode()
+    stdout = utils.run_cmd('docker desktop status --format json', check=False)
+    if not stdout:
+        return 'not running'
+    data = json.loads(stdout)
+    return data['Status']
+
+
+def docker_desktop_version(return_json=False):
+    stdout = subprocess.check_output(utils.fmt_cmd('docker version --format json'))
+    data = json.loads(stdout.decode())
+    cmp = data['Server']['Components']
+    vers = [c['Version'] for c in cmp if c['Name'] == 'Engine'][0]
+    return Version(vers)
+
+def get_latest_docker_version():
+
+    # A bit of a hack: gets version as tag of base docker image (which is for use with "docker in docker")
+
+    s = requests.Session()
+    url = 'https://registry.hub.docker.com/v2/namespaces/library/repositories/docker/tags'
+    tags = []
+    r  = s.get(url, headers={ "Content-Type" : "application/json"})
+    if not r.ok:
+        r.raise_for_status()
+    data = r.json()
+    for entry in data['results']:
+        if 'name' in entry:
+            try:
+                tags.append(Version(entry['name']))
+            except InvalidVersion:
+                # latest and other non-version tags
+                pass
+    return max(tags)
+
+
+def update_docker_desktop(return_json=False):
+
+    if utils.system() == 'Windows':
+        current_engine_version = version()
+        most_recent_version = get_latest_docker_version()
+        if current_engine_version < most_recent_version:
+            utils.boxed_text(f"Update Docker Desktop",
+                             [f'Please open the "Docker Desktop" application and and click where it says "New version available" in the bottom right corner.', 
+                              'Then scroll down and click the blue button to update'],
+                            prompt='Press Enter to close Franklin.', fg='red')
+            sys.exit(0)
+    else:
+        # stdout = subprocess.check_output(utils.format_cmd('docker desktop update --check-only')).decode()
+        stdout = utils.run_cmd('docker desktop update --check-only')
+        if 'is already the latest version' not in stdout:
+            # subprocess.run(utils.format_cmd('docker desktop update --quiet'))
+            utils.run_cmd('docker desktop update --quiet')
---
+++ franklin/terminal.py
@@ -0,0 +1,133 @@
+import shutil
+from .config import WRAP_WIDTH, PG_OPTIONS, MIN_WINDOW_WIDTH, MIN_WINDOW_HEIGHT, BOLD_TEXT_ON_WINDOWS
+import click
+import time
+from . import utils
+from .logger import logger
+from . import terminal as term
+
+def check_window_size():
+
+    def _box(text):
+        window_box = \
+            '|' + '-'*(MIN_WINDOW_WIDTH-2) + '|\n' + \
+        ('|' + ' '*(MIN_WINDOW_WIDTH-2) + '|\n') * (MIN_WINDOW_HEIGHT-3) + \
+            '| ' + text.ljust(MIN_WINDOW_WIDTH-3) + '|\n' + \
+            '|' + '-'*(MIN_WINDOW_WIDTH-2) + '|' 
+        return '\n'*150 + window_box
+
+
+    ts = shutil.get_terminal_size()
+    if ts.columns < MIN_WINDOW_WIDTH or ts.lines < MIN_WINDOW_HEIGHT:
+        while True:
+            ts = shutil.get_terminal_size()
+            if ts.columns >= MIN_WINDOW_WIDTH and ts.lines >= MIN_WINDOW_HEIGHT:
+                break
+            click.secho(_box('Please resize the window to at least fit this square'), fg='red', bold=True)
+            time.sleep(0.1)
+        click.secho(_box('Thanks!'), fg='green', bold=True)
+        click.pause()
+
+    text = 'Please resize the window to at least fit this square'
+
+
+def dummy_progressbar(seconds, label='Hang on...', **kwargs):
+    pg_options = PG_OPTIONS.copy()
+    pg_options.update(kwargs)
+    with click.progressbar(length=100, label=label, **pg_options) as bar:
+        for i in range(100):
+            time.sleep(seconds/100)
+            bar.update(1)
+
+
+def wrap(text, width=None, indent=True, initial_indent=None, subsequent_indent=None):
+    """
+    Wraps text to fit terminal width or WRAP_WIDTH, whatever
+    is smaller
+    """
+    if width is None:
+        width = WRAP_WIDTH
+
+    nr_leading_nl = len(text) - len(text.lstrip('\n'))
+    text = text.lstrip('\n')
+    
+    if initial_indent is None:
+        initial_indent = text[:len(text) - len(text.lstrip())]
+    text = text.lstrip()
+
+    if subsequent_indent is None:
+        subsequent_indent = initial_indent
+
+    trailing_ws = text[len(text.rstrip()):]   
+    text = text.rstrip()
+
+    if not indent:
+        initial_indent = ''
+        subsequent_indent = ''
+
+    text = click.wrap_text(text, width=max((shutil.get_terminal_size().columns)/2, width), 
+                initial_indent=initial_indent, subsequent_indent=subsequent_indent, 
+                preserve_paragraphs=True)
+    
+    text = '\n' * nr_leading_nl + text + trailing_ws
+    return text
+
+
+def secho(text='', width=None, center=False, nowrap=False, log=True,
+          indent=True, initial_indent=None, subsequent_indent=None, **kwargs):
+    """
+    Wrapper for secho that wraps text.
+    kwargs are passed to click.secho
+    """
+    if width is None:
+        width = WRAP_WIDTH
+    if not nowrap:
+        text = wrap(text, width=width, 
+                    indent=indent,
+                    initial_indent=initial_indent, 
+                    subsequent_indent=subsequent_indent)
+    if center:
+        cent = []
+        for line in text.strip().splitlines():
+            line = line.strip()
+            if line:
+                line = line.center(width)
+            cent.append(line)
+        text = '\n'.join(cent)        
+    if log:
+        for line in text.strip().splitlines():
+            try:
+                logger.debug(line.strip())
+            except UnicodeEncodeError:
+                line = str.decode('utf-8',errors='ignore')
+                logger.debug(line.strip())
+                
+
+                pass
+    if utils.system() == 'Windows' and not BOLD_TEXT_ON_WINDOWS:
+        kwargs['bold'] = False
+    click.secho(text, **kwargs)
+
+
+def echo(text='', width=None, nowrap=False, log=True, indent=True, 
+         initial_indent=None, subsequent_indent=None, **kwargs):
+    
+    secho(text, width=width, nowrap=nowrap, log=log, indent=indent, 
+          initial_indent=initial_indent, subsequent_indent=subsequent_indent, **kwargs)
+
+
+def boxed_text(header, lines=[], prompt='', **kwargs):
+    term.echo()
+    term.secho(f"{header}:", **kwargs)
+    term.secho('='*WRAP_WIDTH, **kwargs)
+    term.echo()
+    for line in lines:
+        term.echo(f"  {line}")
+    term.echo()
+    term.echo(f"  {prompt}")
+    term.echo()
+    term.secho('='*WRAP_WIDTH, **kwargs)
+    term.echo()
+    if prompt:
+        click.pause('')
+
