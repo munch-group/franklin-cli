e061b47670f5a36c9b6e3931bd5000fcd328278e
Kasper Munch <kaspermunch@birc.au.dk>
2025-03-31 09:35:59 +0200

    Moved scripts

--- docs/docs-build-render.sh
+++ docs/docs-build-render.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-cd docs
 rm -f api/_styles-quartodoc.css api/_sidebar.yml *.qmd
 quartodoc build && quartodoc interlinks && quarto render
-cd ..
--- docs/docs-run-notebooks.sh
+++ docs/docs-run-notebooks.sh
@@ -3,7 +3,7 @@
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
-for FILE in docs/pages/*.ipynb ; do
+for FILE in pages/*.ipynb ; do
     echo -e "${BLUE}Rendering ${FILE}${NC}"
     CMD="jupyter nbconvert --Application.log_level=50 --to notebook --execute --inplace $FILE"
     echo $CMD
