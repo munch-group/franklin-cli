bea6ada930e9aaad0874ff799fc5db4a5ae20e5f
Kasper Munch <kaspermunch@birc.au.dk>
2025-05-31 10:14:54 +0200

    update

--- src/franklin/config.py
+++ src/franklin/config.py
@@ -1,14 +1,19 @@
 
 
+name = 'franklin'
 maintainer_email = 'kaspermunch@birc.au.dk'
-conda_channel = 'munch-group'
+
 gitlab_domain = 'gitlab.au.dk'
-registry_base_url = 'registry.gitlab.au.dk'
-documentation_url = 'https://munch-group.org/franklin'
-gitlab_group = 'franklin'
-gitlab_api_url = 'https://gitlab.au.dk/api/v4'
+gitlab_group = name
+gitlab_api_url = f'https://{gitlab_domain}/api/v4'
 gitlab_token = 'glpat-8F4yGmS6v_xZyqzyyoUM'
+registry_base_url = f'registry.{gitlab_domain}'
+
+conda_channel = 'munch-group'
+documentation_url = 'https://munch-group.org/{name}'
+
 required_gb_free_disk = 5.0
+
 allow_subdirs = False
 wrap_width = 75
 min_window_width = 80
@@ -16,6 +21,7 @@ min_window_height = 24
 bold_text_on_windows = False
 pg_options = dict(fill_char='=', empty_char=' ', width=36, show_eta=False)
 pg_ljust = 30
+
 docker_settings = {
             "AutoDownloadUpdates": True,
             "AutoPauseTimedActivitySeconds": 30,
