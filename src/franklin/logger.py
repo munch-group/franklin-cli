120adfbac1f4f79781798b9d61c23f072bc57f3b
Kasper Munch <kaspermunch@birc.au.dk>
2025-06-12 14:32:44 +0200

    update

--- src/franklin/logger.py
+++ src/franklin/logger.py
@@ -1,3 +1,4 @@
+import sys
 import logging
 from importlib.metadata import version
 ver = version(__package__)
@@ -16,3 +17,22 @@ logger.setLevel(logging.DEBUG)
 # logger.warning('This is a warning message')
 # logger.error('This is an error message')
 # logger.critical('This is a critical message')
+
+class LoggerWriter:
+    def __init__(self, level):
+        # self.level is really like using log.debug(message)
+        # at least in my case
+        self.level = level
+
+    def write(self, message):
+        # if statement reduces the amount of newlines that are
+        # printed to the logger
+        if message != '\n':
+            self.level(message)
+
+    def flush(self):
+        # create a flush method so things can be flushed when
+        # the system wants to. Not sure if simply 'printing'
+        # sys.stderr is the correct way to do it, but it seemed
+        # to work properly for me.
+        self.level(sys.stderr)
