03bd83bcdae5f07373515092690b6f68533459c9
Kasper Munch <kaspermunch@birc.au.dk>
2025-06-13 13:07:25 +0200

    update

--- release-tag.sh
+++ release-tag.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 if [[ `git status --porcelain` ]]; then
-  RED='\033[0;31m'
-  NC='\033[0m' # No Color
   echo -e "${RED}Changes to pyproject.toml must be pushed first.${NC}"
 else
   v=$(python setup.py --version) || exit
   git tag -a "v${v}" -m "${1:-Release}" && git push origin --tags
-  GREEN='\033[0;32m'
-  NC='\033[0m' # No Color
-  echo -e "${RED}Changes to pyproject.toml must be pushed first.${NC}"
+  echo -e "${GREEN}Changes to pyproject.toml must be pushed first.${NC}"
 fi
