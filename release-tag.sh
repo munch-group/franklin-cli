abd0d338ec2af1cbbf7f35cb35333f0505b34b86
Kasper Munch <kaspermunch@birc.au.dk>
2025-06-13 13:01:03 +0200

    update

--- release-tag.sh
+++ release-tag.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 if [[ `git status --porcelain` ]]; then
+  RED='\033[0;31m'
+  NC='\033[0m' # No Color
+  echo -e "${RED}Changes to pyproject.toml must be pushed first.${NC}"
   echo "Changes to pyproject.toml must be pushed first."
 else
   v=$(python setup.py --version) || exit
